c7acf4380e97b9987a5c3da2fc0bc9180a55375e
Cixo <cixolaptop@gmail.com>
2023-01-26 00:48:06 +0100

    Fix arm build

--- build-gcc-arm/build.sh
+++ build-gcc-arm/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SOURCE=../sources/sensor.c
-TARGET=../build-linux-gcc/libZPH01.a
-OBJECT=../build-linux-gcc/ZPH01lib.o
+TARGET=../build-gcc-arm/libZPH01.a
+OBJECT=../build-gcc-arm/ZPH01lib.o
 
 arm-none-eabi-gcc -mtune=cortex-m0plus -O2 $SOURCE -c -o $OBJECT
 arm-none-eabi-ar -cq $TARGET $OBJECT 
